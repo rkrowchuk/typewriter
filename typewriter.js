64637d19d3c1f78e32a9da3ffdf5475fad463f2c
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-02-15 14:20:42 -0800

    first commit

---
+++ typewriter.js
@@ -0,0 +1,13 @@
+const sentence = "hello there from lighthouse labs";
+
+for(let i = 0; i < sentence.length; i++) {
+  setTimeout(()=>{
+    process.stdout.write(sentence[i]);
+    if(i === sentence.length - 1){ 
+      process.stdout.write('\n');
+   }
+  },(i + 1) * 50);
+}
+
+
+
